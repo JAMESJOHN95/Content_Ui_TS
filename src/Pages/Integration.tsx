eae26183e0bca44ebe5bd7c29bf444d80f21720d
Naviya v i <naviyaisahac@gmail.com>
2025-04-08 12:57:30 +0530

    Integration page updated

--- src/Pages/Integration.tsx
+++ src/Pages/Integration.tsx
@@ -6,19 +6,19 @@ function Integration() {
       <div className="col-md-1">
         <Layout />
       </div>
-      <div className="col-md-11 container">
-        <div className="mt-3 container">
+      <div className="col-md-11 container" style={{marginTop:'.5rem'}}>
+        <div className="mt-4 container ms-4">
           <h3>External Integration</h3>
           <hr />
         </div>
-        <div className="row border p-0 m-3">
-          <div className="border border-solid p-2 m-2">
+        <div className="row border m-3 container">
+          <div className="border border-solid p-2 m-2 mt-3 p-2">
             <h5>Adobe Journey Optimizer</h5>
           </div>
           <div className="border border-solid p-2 m-2">
             <h5>Salesforce Content Builder</h5>
           </div>
-          <div className="border border-solid p-2 m-2">
+          <div className="border border-solid p-2 m-2 mb-3">
             <h5>AWS Pinpoint</h5>
           </div>
         </div>
