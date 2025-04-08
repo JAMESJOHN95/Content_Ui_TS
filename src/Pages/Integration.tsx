ab962c565df80ee8a891a24356859cc27eca941f
Naviya v i <naviyaisahac@gmail.com>
2025-04-08 11:22:32 +0530

    Integration page updated

--- src/Pages/Integration.tsx
+++ src/Pages/Integration.tsx
@@ -4,12 +4,12 @@ function Integration() {
   return (
 <div className='row'>
     <div className='col-md-1'><Layout/></div>
-        <div className='col-md-11 container'>
-         <div className='mt-5 container'> 
+        <div className='col-md-11'>
+         <div className='mt-5'> 
             <h3>External Integration</h3>
             <hr/>
          </div>
-    <div className='row border p-5 m-3'>
+    <div className='row border p-5 m-1'>
         
                  <div className='border border-solid p-2 m-2'><h5 >Adobe Journey Optimizer</h5></div>
                  <div className='border border-solid p-2 m-2'><h5 >Salesforce Cloud</h5></div>
